e0c82125855fede091660073ec889c1212a1cd24
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-10 16:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,20 +25,12 @@ function add2(n) {
   return function() {
    
    var theFunk = funkyFunction()
-    return "FUNKY"
+     return "FUNKY"
     
   }
 }
 
-function outerFunction() {
-  var innerVariable = "I'm sort of a secret.";
- 
-  return function innerScope() {
-    var inaccessible = "Nothing can touch me.";
- 
-    return innerVariable;
-  }
-}
+
 
 
 
